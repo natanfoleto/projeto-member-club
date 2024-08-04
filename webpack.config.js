52f6a32ffe8ffdd42e81aab57a8b2f767c76521e
“Natan <natanfoleto2015@hotmail.com>
2024-08-04 17:18:39 -0300

    Adicionando webpack server na aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist")
+	},
+	
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "dist")
+		},
+		port: 3000,
+		open: true,
+		liveReload: true,
 	}
 }
