b226ffb1770ed8156a3a70aa8365032c7babd066
“Natan <natanfoleto2015@hotmail.com>
2024-08-04 17:20:39 -0300

    Carregando o HTML no build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
 	target: "web",
@@ -8,7 +9,6 @@ module.exports = {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist")
 	},
-	
 	devServer: {
 		static: {
 			directory: path.resolve(__dirname, "dist")
@@ -16,5 +16,11 @@ module.exports = {
 		port: 3000,
 		open: true,
 		liveReload: true,
-	}
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "index.html")
+		})
+	]
 }
