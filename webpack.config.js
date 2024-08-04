dcb4eb8b6a33bccf5eb986475c0cc59164714b04
“Natan <natanfoleto2015@hotmail.com>
2024-08-04 18:14:27 -0300

    Copiando os assets na aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
 	target: "web",
@@ -20,6 +21,14 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, "index.html"),
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "src", "assets"),
+					to: path.resolve(__dirname, "dist", "src", "assets"),
+				}
+			]
 		})
 	],
 	
